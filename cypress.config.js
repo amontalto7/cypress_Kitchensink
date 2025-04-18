14a9b1dbb93e66299a8864983c0dc1641b27c53c
Anthony Montalto <37939663+amontalto7@users.noreply.github.com>
2025-04-18 18:00:15 -0400

    fixed

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+import { defineConfig } from "cypress";
+
+export default {
   'projectId': '4b7344',
   e2e: {
-      "baseUrl" : "http://localhost:8080"
+      "baseUrl" : "http://localhost:8080",
+      specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+      supportFile: 'cypress/support/e2e.js', // adjust if needed
   },
 
 }
